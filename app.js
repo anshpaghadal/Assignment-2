3563cbe154a21e9a82948f3345291350d2f747dc
Ansh <122945925+anshpaghadal@users.noreply.github.com>
2024-08-09 19:31:33 -0400

    port changed again

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
